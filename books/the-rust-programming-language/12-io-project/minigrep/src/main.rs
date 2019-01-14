a5a73a99fce01814fdc695d9768b93576e407507
Darren Beukes <darren.beukes@gmail.com>
2019-01-15 09:10:06 +1100

    Improve IO project with iterators

--- books/the-rust-programming-language/12-io-project/minigrep/src/main.rs
+++ books/the-rust-programming-language/12-io-project/minigrep/src/main.rs
@@ -5,11 +5,9 @@ use minigrep;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);   
+        process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
