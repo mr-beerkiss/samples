01e633b517590f43a2c81c63f1843a75cda89a52
Darren Beukes <code@darrenbeukes.com>
2020-05-17 20:31:00 +1000

    Latest

--- udemy/Unreal Engine C++ Developer/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ udemy/Unreal Engine C++ Developer/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,20 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    // NOTE: _ALL_ string literals should be wrapped in the `TEXT` macro... not just assignments
+    // to `FString`
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cows!"));
+    PrintLine(TEXT("Bull is right letter in the right place."));
+    PrintLine(TEXT("Cow is right letter in the wrong place."));
+    PrintLine(TEXT("Press enter to continue"));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-
+    // FString literals are usually encoded as ANSI and need to re-encoded to TChar strings which
+    // are platform specific unicode strings.
+    // Use the `TEXT` macro instead of assign string literals
+    FString HiddenWord = TEXT("candor");
+    ClearScreen();
 }
