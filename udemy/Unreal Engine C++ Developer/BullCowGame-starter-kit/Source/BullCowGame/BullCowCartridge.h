7483cb28ea10e5099da5e3e7e951228e60634dc8
Darren Beukes <code@darrenbeukes.com>
2020-05-24 01:17:14 +1000

    Latest samples

--- udemy/Unreal Engine C++ Developer/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ udemy/Unreal Engine C++ Developer/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -2,31 +2,36 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+
 #include "Console/Cartridge.h"
+#include "CoreMinimal.h"
+#include "FileHelper.h"
+#include "Paths.h"
+
 #include "BullCowCartridge.generated.h"
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class BULLCOWGAME_API UBullCowCartridge : public UCartridge
-{
-	GENERATED_BODY()
+class BULLCOWGAME_API UBullCowCartridge : public UCartridge {
+  GENERATED_BODY()
 
 public:
-	virtual void BeginPlay() override;
-	virtual void OnInput(const FString &Input) override;
-	void InitGame();
-	void PrintHint();
-	void EndGame(const bool Winner);
-	static bool IsIsogram(const FString &Word);
-	TArray<FString> AnalyseWord(const FString &Word) const;
+  virtual void BeginPlay() override;
+  virtual void OnInput(const FString& Input) override;
+  void InitGame();
+  void PrintHint();
+  void EndGame(const bool Winner);
+  static bool IsIsogram(const FString& Word);
+  TArray<FString> AnalyseWord(const FString& Word) const;
+	void AnalyseWord(const FString& Word, int32& Bulls, int32& Cows) const;
 
-	// Your declarations go below!
+  // Your declarations go below!
 private:
-	FString SecretWord;
-	TMap<char, int32> LettersAndIndexes;
-	TArray<FString> SecretWordList;
-	int32 SecretWordListSize;
-	int32 Lives;
-	int32 Hints;
-	bool GameOver;
+  FString SecretWord;
+  TMap<char, int32> LettersAndIndexes;
+  TArray<FString> SecretWordList;
+	TArray<int32> HintsShown;
+  int32 SecretWordListSize;
+  int32 Lives;
+  int32 Hints;
+  bool GameOver;
 };
